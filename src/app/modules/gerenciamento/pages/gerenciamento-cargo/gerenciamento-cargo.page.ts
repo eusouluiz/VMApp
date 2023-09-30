702eca6cc2b19f99d578af595ad3ad39444a6a78
Gabriel F. Gomes <gabrielfalavinhagomes@gmail.com>
2023-09-30 11:29:19 -0300

    gerenciamento cargos

--- src/app/modules/gerenciamento/pages/gerenciamento-cargo/gerenciamento-cargo.page.ts
+++ src/app/modules/gerenciamento/pages/gerenciamento-cargo/gerenciamento-cargo.page.ts
@@ -1,4 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+export interface Funcionalidade {
+  idFuncionalidade: String,
+  nome: String,
+}
+
+export interface Cargo {
+  idCargo: Number,
+  nome: String,
+  funcionalidades: Funcionalidade[]
+}
+
+var a1: Funcionalidade = {idFuncionalidade: 'funcionalidade1', nome: 'nome1'}
+var a2: Funcionalidade = {idFuncionalidade: 'funcionalidade2', nome: 'nome2'}
+
+var RESPONSAVEL_DATA: Cargo[] = [
+  {
+    idCargo: 0,
+    nome: 'Cargo1',
+    funcionalidades: [a1, a2]
+  },
+  {
+    idCargo: 1,
+    nome: 'Cargo2',
+    funcionalidades: [a1]
+  },
+  {
+    idCargo: 2,
+    nome: 'Cargo3',
+    funcionalidades: [a2]
+  }
+]
 
 @Component({
   selector: 'app-gerenciamento-cargo',
@@ -7,9 +41,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GerenciamentoCargoPage implements OnInit {
 
-  constructor() { }
+  cargos: any
+
+  constructor(
+    private router: Router,
+    ) { 
+  }
 
   ngOnInit() {
+
+    this.cargos = RESPONSAVEL_DATA
+  }
+
+  public navegarDetalheCargo(cargo: Cargo){
+    const caminho: String = '/cargo/' + cargo.idCargo + '/detalhes'
+    this.navegarPara(caminho)
+  }
+  
+  public navegarCadastroCargo(){
+    const caminho: String = '/cargo/cadastro'
+    this.navegarPara(caminho)
+  }
+
+  private navegarPara(rota: String){
+    if (rota.substring(0, 1) !== '/') {
+      rota = '/' + rota
+    }
+    const caminho: String = '/app/gerenciamento' + rota
+    this.router.navigate([caminho])
   }
 
 }
