03f6c42e36c822d90ad965761989c35be03c2277
Gabriel F. Gomes <gabrielfalavinhagomes@gmail.com>
2023-10-01 12:30:02 -0300

    Autenticacao e busca de usuario

--- src/app/modules/login/pages/login/login.page.ts
+++ src/app/modules/login/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AppModule, Funcionario, Responsavel, Usuario } from '../../../../app.module';
 
 @Component({
   selector: 'app-login',
@@ -16,24 +17,106 @@ export class LoginPage implements OnInit {
       private router: Router,
     ) {
     this.form = this.formBuilder.group({
-      login: ['', Validators.required],
+      cpf: ['', Validators.required],
       senha: ['', Validators.required]
     });
+    console.log(AppModule.usuarioLogado)
   }
+
   ngOnInit() {
   }
-  
 
   submit() {
-    console.log(this.form.value)
-    console.log(this.form)
-    const loginForm = this.form.value.login
+    const cpfForm = this.form.value.cpf
     const senhaForm = this.form.value.senha
-    if(loginForm === 'login' && senhaForm === 'senha123'){
-      console.log('logado com sucesso')
-      this.router.navigate(['/app'])
-    } else {
-      console.log('login ou senha invalida')
+
+    try {
+      const usuario = autenticar(cpfForm, senhaForm)
+
+      if(usuario){
+        console.log('logado com sucesso')
+        switch (usuario.tipoUsuario){
+          case 'F': {
+            console.log('funcionario')
+            const funcionario = buscarFuncionario(usuario.idUsuario)
+            if (funcionario === undefined) {
+              throw new Error('Funcionario nao encontrado')
+            }
+            definirUsuarioLogado(funcionario)
+            break
+          }
+          case 'R': {
+            console.log('responsavel')
+            const responsavel = buscarResponsavel(usuario.idUsuario)
+            if (responsavel === undefined) {
+              throw new Error('Responsavel nao encontrado')
+            }
+            definirUsuarioLogado(responsavel)
+            break
+          }
+          case 'A': {
+            console.log('ambos')
+            break
+          }
+          default: {
+            throw new Error('Usuario nao definido o tipo')
+          }
+        }
+        console.log(AppModule.usuarioLogado)
+        this.navegaParaApp()
+      } 
+    } catch (e: any) {
+      console.log(e.message)
     }
   }
+
+  private navegaParaApp(){
+    this.router.navigate(['/app'])
+  }
+
 }
+
+
+function autenticar(cpfForm: String, senhaForm: String): Usuario {
+  const usuario = buscarUsuarioPorCpf(cpfForm)
+  if (usuario === undefined){
+    throw new Error("Usuario nao encontrado")
+  }
+  if(isSenhaCorreta(usuario, senhaForm)){
+    return usuario
+  } else {
+    throw new Error("Senha incorreta")
+  }
+}
+
+function buscarUsuarioPorCpf(cpfForm: String): Usuario | undefined {
+  const listaUsuarios = AppModule.listaUsuarios
+
+  return listaUsuarios.find((u) => {
+    return u.cpf === cpfForm
+  })
+}
+
+function isSenhaCorreta(usuario: Usuario, senhaForm: String) {
+  return usuario.senha === senhaForm
+}
+
+function buscarResponsavel(idUsuario: Number): Responsavel | undefined {
+  const listaResponsaveis = AppModule.listaResponsaveis
+
+  return listaResponsaveis.find((r) => {
+    return r.usuario.idUsuario === idUsuario
+  })
+}
+
+function buscarFuncionario(idUsuario: Number): Funcionario | undefined {
+  const listaFuncionarios = AppModule.listaFuncionarios
+
+  return listaFuncionarios.find((f) => {
+    return f.usuario.idUsuario === idUsuario
+  })
+}
+function definirUsuarioLogado(usuarioLogado: Responsavel | Funcionario) {
+  AppModule.usuarioLogado = usuarioLogado
+}
+
