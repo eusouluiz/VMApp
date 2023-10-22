d58f89dd90d56ba84e3eb93bb13c19ea7c28afb7
Gabriel F. Gomes <gabrielfalavinhagomes@gmail.com>
2023-10-22 18:34:02 -0300

    Adequando telas funcionario

--- src/app/modules/gerenciamento/pages/gerenciamento-funcionario-detalhes/gerenciamento-funcionario-detalhes.page.ts
+++ src/app/modules/gerenciamento/pages/gerenciamento-funcionario-detalhes/gerenciamento-funcionario-detalhes.page.ts
@@ -1,131 +1,90 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { MatTable } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common'
-import { CARGO_DATA, Cargo, FUNCIONARIO_DATA, Funcionario, funcionarioVazio } from '../../../../shared/utilities/entidade/entidade.utility';
+import { Cargo, Funcionario, funcionarioVazio } from '../../../../shared/utilities/entidade/entidade.utility';
+import { PaginaGerenciamento } from '../../../../shared/utilities/pagina-gerenciamento/pagina-gerenciamento.utility';
+import { FuncionarioService } from '../../../../core/services/funcionario-service/funcionario.service';
+import { CargoService } from '../../../../core/services/cargo-service/cargo.service';
+import { ConstantesRotas } from '../../../../shared/utilities/constantes/constantes.utility';
 
 @Component({
   selector: 'app-gerenciamento-funcionario-detalhes',
   templateUrl: './gerenciamento-funcionario-detalhes.page.html',
   styleUrls: ['./gerenciamento-funcionario-detalhes.page.scss'],
 })
-export class GerenciamentoFuncionarioDetalhesPage implements OnInit {
-
-  modo: 'cadastrar' | 'editar' | 'detalhes' = 'detalhes'
+export class GerenciamentoFuncionarioDetalhesPage extends PaginaGerenciamento implements OnInit {
 
   funcionario: Funcionario
-
-  form: UntypedFormGroup | undefined;
+  listaTodosCargos: Cargo[] | null = null
 
   constructor(
     private formBuilder: UntypedFormBuilder,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private location: Location
-    ) { 
-      console.log(this.activatedRoute.snapshot.paramMap.get('id'))
-      console.log(this.router.url.split('/').pop())
+    public location: Location,
+    private funcionarioService: FuncionarioService,
+    private cargoService: CargoService,
+  ) {
+    const ROTA_BASE = ConstantesRotas.ROTA_APP + ConstantesRotas.ROTA_GERENCIAMENTO
+    super(router, ROTA_BASE, location)
 
-      this.definirModo()
-      
-      this.inicializarFormBuscaCargo()
-      if (this.isModoDetalhes()) {
-        var id = Number(this.activatedRoute.snapshot.paramMap.get('id'))
-        this.funcionario = this.resgatarFuncionario(id)
-        this.inicializarTabelaCargos()
-      } else {
-        this.funcionario = funcionarioVazio()
-        this.inicializarTabelaCargos()
-      }
+    this.definirModo()
 
-      this.form = this.formBuilder.group({
-        nome: [this.funcionario.nome, Validators.required],
-        telefone: [this.funcionario.usuario.telefone, Validators.required],
-        cpf: [this.funcionario.usuario.cpf, Validators.required],
-        senha: [this.funcionario.usuario.senha, Validators.required],
-      })
-      if (this.isModoDetalhes()) {
-        this.form.disable()
-      }
+    this.inicializarFormBuscaCargo()
+    const id = this.activatedRoute.snapshot.paramMap.get('id')
+    if (this.isModoDetalhes() && id !== null) {
+      this.funcionario = this.resgatarFuncionario(Number.parseInt(id))
+      this.inicializarTabelaCargos()
+    } else {
+      this.funcionario = funcionarioVazio()
+      this.inicializarTabelaCargos()
+    }
+
+    this.form = this.formBuilder.group({
+      nome: [this.funcionario.nome, Validators.required],
+      telefone: [this.funcionario.usuario.telefone, Validators.required],
+      cpf: [this.funcionario.usuario.cpf, Validators.required],
+      senha: [this.funcionario.usuario.senha, Validators.required],
+    })
+
+    if (this.isModoDetalhes()) {
+      this.form.disable()
+    }
   }
 
   ngOnInit() {
   }
 
-  // ---- controle modo pagina ----//
-
-  private definirModo(){
-    // pega ultimo termo do endpoint
-    const rota = this.router.url.split('/').pop()
-
-    if (rota === 'cadastro') {
-      this.modo = 'cadastrar'
-    }
-  }
-
-  isModoDetalhes(){
-    return this.modo === 'detalhes'
-  }
-
-  isModoEditar(){
-    return this.modo === 'editar'
-  }
-
-  isModoCadastrar(){
-    return this.modo === 'cadastrar'
-  }
-
-  // ---- define modo pagina ----//
-
   // ---- busca funcionario ----//
-  private resgatarFuncionario(id: Number): Funcionario{
-    for (let i = 0; i < FUNCIONARIO_DATA.length; i++) {
-      const funcionario = FUNCIONARIO_DATA[i];
-      if (funcionario.idFuncionario === id) {
-        return funcionario
-      }
+  private resgatarFuncionario(id: number): Funcionario {
+    const funcionario = this.funcionarioService.buscarFuncionario(id)
+    if (funcionario !== undefined) {
+      return funcionario
     }
     return funcionarioVazio()
   }
   // ---- busca funcionario ----//
 
-  
   // ---- controle botoes ----//
 
-  eventoActions(ev:any){
-    if (ev.detail.data === undefined) {
-      return
-    }
-    const action = ev.detail.data.action
-    console.log(action)
-
-    if(action === 'delete'){
-      this.deletarFuncionario()
-    }
+  //delecao
+  protected deletar() {
+    this.funcionarioService.deletarFuncionario(this.funcionario.idFuncionario)
+    this.retornarPagina()
   }
 
-  private deletarFuncionario(){
-    console.log('funcionario deletado')
-    this.retornaPagina()
-  }
+  //edicao
 
-  //editar
-  iniciarEdicao(){
-    console.log('edicao iniciada')
-    console.log(this.form)
-    this.modo = 'editar'
-    this.form?.enable()
-
+  protected inicializarComponentesEdicao() {
     this.inicializarTabelaCargos()
   }
 
   //cancelar edicao
-  cancelar(){
-    console.log('cancelado')
+  cancelar() {
 
     if (this.isModoCadastrar()) {
-      this.retornaPagina()
+      this.retornarPagina()
       return
     }
 
@@ -140,34 +99,36 @@ export class GerenciamentoFuncionarioDetalhesPage implements OnInit {
     this.form?.disable()
 
     this.inicializarTabelaCargos()
-    this.tabelaCargos.renderRows()
   }
 
   //salvar edicao
-  salvar(){
-    console.log('salvado')
-    
-    console.log('nome: ' + this.form?.value.nome)
-    console.log('telefone: ' + this.form?.value.telefone)
-    console.log('cpf: ' + this.form?.value.cpf)
-    console.log('cpf: ' + this.form?.value.senha)
+  salvar() {
+    if (this.form?.valid) {
+      this.funcionario.nome = this.form?.value.nome
+      this.funcionario.usuario.telefone = this.form?.value.telefone
+      this.funcionario.usuario.cpf = this.form?.value.cpf
+      this.funcionario.usuario.senha = this.form?.value.senha
 
-    this.funcionario.nome = this.form?.value.nome
-    this.funcionario.usuario.telefone = this.form?.value.telefone
-    this.funcionario.usuario.cpf = this.form?.value.cpf
-    this.funcionario.usuario.senha = this.form?.value.senha
+      this.atualizarCargos()
 
-    this.modo = 'detalhes'
-    this.form?.disable()
+      if (this.isModoCadastrar()) {
+        this.funcionarioService.incluirFuncionario(this.funcionario)
+      } else {
+        this.funcionarioService.alterarFuncionario(this.funcionario)
+      }
 
-
+      this.modo = 'detalhes'
+      this.form?.disable()
+    } else {
+      this.form?.markAllAsTouched()
+    }
   }
   // ---- controle botoes ----//
 
   // ---- controle cargos ----//
 
   formBuscaCargo!: UntypedFormGroup
-  
+
   inicializarFormBuscaCargo() {
     this.formBuscaCargo = this.formBuilder.group({
       busca: ''
@@ -175,46 +136,73 @@ export class GerenciamentoFuncionarioDetalhesPage implements OnInit {
   }
 
   //nome colunas
-  colunasCargo: string[] = ['nome', 'acao']
-  listaCargosBusca: Cargo[] = CARGO_DATA.slice()
-  nomeCargosBusca: String[] = this.getNomeCargosBusca(this.listaCargosBusca)
+  listaCargosBusca: Cargo[] = []
+  nomeCargosBusca: string[] = []
 
-  listaCargosTabela!: Cargo[]
+  listaCargosTabela: Cargo[] = []
 
-  @ViewChild(MatTable)
-  tabelaCargos!: MatTable<Cargo>;
+  private inicializarTabelaCargos() {
+    this.listaCargosTabela = []
+    if (this.funcionario.cargo.nome !== '') {
+      this.listaCargosTabela.push(this.funcionario.cargo)
+    }
+    if (!this.isModoDetalhes()) {
+      this.inicializarBuscaCargos()
+    }
+  }
 
-  private inicializarTabelaCargos(){
-    this.listaCargosTabela = [this.funcionario.cargo]
-    this.listaCargosBusca = CARGO_DATA.slice()
-    this.nomeCargosBusca = this.getNomeCargosBusca(this.listaCargosBusca)
+  private inicializarBuscaCargos() {
+    // evitar com que lista de todos os cargos seja buscada toda hora
+    if (this.listaTodosCargos === null) {
+      this.listaTodosCargos = this.cargoService.buscarTodosCargos().slice()
+    }
+
+    this.listaCargosBusca = []
+    this.listaTodosCargos.forEach((c) => {
+      const idCargo = c.idCargo
+      var isFuncionarioPossuiCargo = false
+
+      for (let i = 0; i < this.listaCargosTabela.length; i++) {
+        const funcionarioCargo = this.listaCargosTabela[i];
+        if (funcionarioCargo.idCargo === idCargo) {
+          isFuncionarioPossuiCargo = true
+          break
+        }
+      }
+
+      if (!isFuncionarioPossuiCargo) {
+        this.listaCargosBusca.push(c)
+      }
+    })
+
+    this.nomeCargosBusca = this.resgatarNomeCargosBusca(this.listaCargosBusca)
     this.limparCampoBusca()
   }
 
-  private getNomeCargosBusca(lista: Cargo[]): String[]{
-    var nomes: String[] = []
+  private resgatarNomeCargosBusca(lista: Cargo[]): string[] {
+    var nomes: string[] = []
     lista.forEach(cargo => {
       nomes.push(cargo.nome)
     });
     return nomes
   }
 
-  adicionarCargo(valor: number){
-    console.log('adicionando cargo')
-    console.log(valor)
+  adicionarCargo(valor: number) {
 
-    if (valor === -1){
+    if (valor === -1) {
       this.navegarTelaCargo(valor)
       return
     }
-    
+
     const cargo = this.listaCargosBusca[valor]
-    console.log(cargo)
 
+    // remove 1 para colocar outro
+    if (this.listaCargosTabela.length > 0){
+      this.deletarCargo()
+    }
     this.listaCargosTabela.push(cargo)
-    this.tabelaCargos.renderRows()
 
-    this.removeCargoDaLista(valor)
+    this.removerCargoDaListaBusca(valor)
     this.limparCampoBusca()
   }
 
@@ -224,9 +212,9 @@ export class GerenciamentoFuncionarioDetalhesPage implements OnInit {
     })
   }
 
-  private removeCargoDaLista(index: number){
+  private removerCargoDaListaBusca(index: number) {
     for (let i = 0; i < this.listaCargosBusca.length; i++) {
-      if (index === i){
+      if (index === i) {
         this.listaCargosBusca.splice(index, 1)
         this.nomeCargosBusca.splice(index, 1)
         break;
@@ -234,40 +222,28 @@ export class GerenciamentoFuncionarioDetalhesPage implements OnInit {
     }
   }
 
-  navegarTelaCargo(id: Number){
-    console.log('navegar tela cargo: ' + id)
-    var rota
-    if (id !== -1){
-      rota = '/cargo/' + id + '/detalhes'
-    } else {
-      rota = '/cargo/cadastro'
-    }
-    this.navegarPara(rota) 
+  private atualizarCargos() {
+    this.funcionario.cargo = this.listaCargosTabela[0]
   }
 
-  deletarCargo(id: Number){
-    console.log('deletar: ' + id)
-    const indexCargo = this.listaCargosTabela.findIndex((a) => {
-      return a.idCargo === id
-    })
-    if (indexCargo !== -1){
-      this.listaCargosTabela.splice(indexCargo, 1)
-      this.tabelaCargos.renderRows()
+  navegarTelaCargo(id: number) {
+    var rota = ConstantesRotas.ROTA_GERENCIAMENTO_CARGO
+    if (id !== -1) {
+      rota = rota + ConstantesRotas.BARRA + id + ConstantesRotas.ROTA_GERENCIAMENTO_DETALHES
+    } else {
+      rota = rota + ConstantesRotas.ROTA_GERENCIAMENTO_CADASTRO
     }
+    this.navegarPara(rota)
+  }
+
+  deletarCargo() {
+    const cargo = this.listaCargosTabela[0]
+    this.listaCargosTabela.splice(0, 1)
+
+    this.listaCargosBusca.push(cargo)
+    this.nomeCargosBusca.push(cargo.nome)
   }
 
   // ---- controle cargos ----//
-
-  private retornaPagina(){
-    this.location.back()
-  }
-  
-  private navegarPara(rota: String){
-    if (rota.substring(0, 1) !== '/') {
-      rota = '/' + rota
-    }
-    const caminho: String = '/app/gerenciamento' + rota
-    this.router.navigate([caminho])
-  }
 
 }
--- src/app/modules/gerenciamento/pages/gerenciamento-funcionario/gerenciamento-funcionario.page.ts
+++ src/app/modules/gerenciamento/pages/gerenciamento-funcionario/gerenciamento-funcionario.page.ts
@@ -1,42 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FUNCIONARIO_DATA, Funcionario } from '../../../../shared/utilities/entidade/entidade.utility';
+import { Funcionario } from '../../../../shared/utilities/entidade/entidade.utility';
+import { Rota } from '../../../../shared/utilities/rota/rota.utility';
+import { FuncionarioService } from '../../../../core/services/funcionario-service/funcionario.service';
+import { Location } from '@angular/common';
+import { ConstantesRotas } from '../../../../shared/utilities/constantes/constantes.utility';
 
 @Component({
   selector: 'app-gerenciamento-funcionario',
   templateUrl: './gerenciamento-funcionario.page.html',
   styleUrls: ['./gerenciamento-funcionario.page.scss'],
 })
-export class GerenciamentoFuncionarioPage implements OnInit {
+export class GerenciamentoFuncionarioPage extends Rota implements OnInit {
 
-  funcionarios: any
+  funcionarios: Funcionario[] = []
+  listaFuncionarios: Funcionario[] = []
+
 
   constructor(
     private router: Router,
-    ) { 
+    private funcionarioService: FuncionarioService,
+    private location: Location,
+  ) {
+    const ROTA_BASE = ConstantesRotas.ROTA_APP + ConstantesRotas.ROTA_GERENCIAMENTO
+    super(router, ROTA_BASE, location)
+    this.inicializarConteudo()
   }
 
   ngOnInit() {
-
-    this.funcionarios = FUNCIONARIO_DATA
   }
 
-  public navegarDetalheFuncionario(funcionario: Funcionario){
-    const caminho: String = '/funcionario/' + funcionario.idFuncionario + '/detalhes'
-    this.navegarPara(caminho)
+  // evento emitido toda vez que retorna a pagina
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event: any) {
+    this.inicializarConteudo()
   }
   
-  public navegarCadastroFuncionario(){
-    const caminho: String = '/funcionario/cadastro'
-    this.navegarPara(caminho)
+  protected inicializarConteudo(): void {
+    this.funcionarios = this.funcionarioService.buscarTodosFuncionarios()
+    this.listaFuncionarios = this.funcionarios.slice()
   }
 
-  private navegarPara(rota: String){
-    if (rota.substring(0, 1) !== '/') {
-      rota = '/' + rota
+  filtarFuncionarioNome(ev: any) {
+    var val = ev.target.value;
+    this.listaFuncionarios = this.funcionarios.slice()
+
+    // se o valor for um valor valido
+    this.listaFuncionarios = this.listaFuncionarios.filter((funcionario) => {
+      return (funcionario.nome.toLowerCase().indexOf(val.toLowerCase()) > -1);
+    })
+  }
+
+  navegarTelaFuncionario(id: number) {
+    var rota = ConstantesRotas.ROTA_GERENCIAMENTO_FUNCIONARIO
+    if (id !== -1) {
+      rota = rota + ConstantesRotas.BARRA + id + ConstantesRotas.ROTA_GERENCIAMENTO_DETALHES
+    } else {
+      rota = rota + ConstantesRotas.ROTA_GERENCIAMENTO_CADASTRO
     }
-    const caminho: String = '/app/gerenciamento' + rota
-    this.router.navigate([caminho])
+    this.navegarPara(rota)
   }
-
 }
