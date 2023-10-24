f453b80c61b94460970d9089da5755f0c5c8a6f3
Gabriel F. Gomes <gabrielfalavinhagomes@gmail.com>
2023-10-24 15:23:47 -0300

    Criacao de listagem de canais

--- src/app/modules/mensagem/pages/mensagem-selecao-canal/mensagem-selecao-canal.page.ts
+++ src/app/modules/mensagem/pages/mensagem-selecao-canal/mensagem-selecao-canal.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Pagina } from '../../../../shared/utilities/pagina/pagina.utility';
+import { Router } from '@angular/router';
+import { ConstantesRotas } from '../../../../shared/utilities/constantes/constantes.utility';
+import { Canal } from '../../../../shared/utilities/entidade/entidade.utility';
+import { CanalService } from '../../../../core/services/canal-service/canal.service';
 
 @Component({
   selector: 'app-mensagem-selecao-canal',
   templateUrl: './mensagem-selecao-canal.page.html',
   styleUrls: ['./mensagem-selecao-canal.page.scss'],
 })
-export class MensagemSelecaoCanalPage implements OnInit {
+export class MensagemSelecaoCanalPage extends Pagina implements OnInit {
 
-  constructor() { }
+  canais: Canal[] = []
+
+  constructor(
+    private router: Router,
+    private canalService: CanalService
+  ) { 
+    const ROTA_BASE = ConstantesRotas.ROTA_APP + ConstantesRotas.ROTA_MENSAGEM
+    super(router, ROTA_BASE)
+
+    this.inicializarConteudo()
+  }
 
   ngOnInit() {
+  }
+
+  protected inicializarConteudo(): void {
+      this.canais = this.canalService.buscarTodosCanais()
   }
 
 }
