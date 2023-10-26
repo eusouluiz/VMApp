7b8e41f596f00a58638087b0aaaa1f6cac2ed6b3
Gabriel F. Gomes <gabrielfalavinhagomes@gmail.com>
2023-10-26 09:09:21 -0300

    apenas mostrando navBar quando n estiver num canal

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { UsuarioLogado } from './../../utilities/usuario-logado/usuario-logado.utility';
 import { Component, Input, OnInit } from '@angular/core';
 
@@ -8,11 +9,26 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  caminhoUrl: string = ''
+
   constructor(
+    private router: Router,
     public usuarioLogado: UsuarioLogado
-  ) { }
+  ) { 
+    router.events.subscribe((e) =>{
+      if (e instanceof NavigationEnd){
+        this.caminhoUrl = router.url
+        console.log(this.caminhoUrl)
+      }
+    })
+  }
 
   ngOnInit() {}
+  
+  isCanalMensagem(){
+    const separacaoCaminhoUrl = this.caminhoUrl.split('/')
+    return separacaoCaminhoUrl.length === 5 && separacaoCaminhoUrl[separacaoCaminhoUrl.length-1] === 'canal'
+  }
 
 
 }
