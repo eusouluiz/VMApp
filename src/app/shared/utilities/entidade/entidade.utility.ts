23cfe8687c6164bed63ac7f7b2c5c4aae3fd7ded
Luiz G. Nascimento <luiznascimento@carbonaut.io>
2023-11-18 02:15:39 -0300

    fix: fix layout padding

--- src/app/shared/utilities/entidade/entidade.utility.ts
+++ src/app/shared/utilities/entidade/entidade.utility.ts
@@ -23,7 +23,7 @@ export var USUARIO_DATA: Usuario[] = [
     tipo: 'F',
   }),
   new Usuario({
-    user_id: "9a9f8466-1760-4ed2-aa57-f691a8547d5e",
+    user_id: '9a9f8466-1760-4ed2-aa57-f691a8547d5e',
     nome: 'Gabriel Falavinha Gomes',
     cpf: '12345678900',
     telefone: '041987654321',
@@ -122,7 +122,7 @@ export var ALUNO_DATA: Aluno[] = [
 
 export var RESPONSAVEL_DATA: Responsavel[] = [
   new Responsavel({
-    responsavel_id: "9a9f8466-1bb8-46ea-8318-58b68ad8f328",
+    responsavel_id: '9a9f8466-1bb8-46ea-8318-58b68ad8f328',
     user_id: USUARIO_DATA[1].user_id,
     updated_at: new Date(),
     created_at: new Date(),
@@ -398,7 +398,7 @@ export var AVISO_RESPONSAVEL_DATA: AvisoResponsavel[] = [
     responsavel_id: RESPONSAVEL_DATA[2].responsavel_id,
     ind_visualizacao: false,
   }),
-]
+];
 
 export var LEMBRETE_DATA: Lembrete[] = [];
 
