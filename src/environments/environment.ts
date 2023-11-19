0ddf2990e0ce4aa5bad477f45bf23b632945b0cc
Gabriel F. Gomes <gabrielfalavinhagomes@gmail.com>
2023-11-18 23:20:30 -0300

    Integrando com Supabase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,6 +14,8 @@ export const environment = {
     payload: {},
   },
   availableLanguages: ['br', 'en'],
+  supabaseUrl: 'https://uatspjoodeqhbhubdgrh.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhdHNwam9vZGVxaGJodWJkZ3JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIwNDYyODIsImV4cCI6MjAwNzYyMjI4Mn0.nCCmUhIAtrW0csSC6y_erJ_H7UpaGxXHC6ON4AYk7tY',
 };
 
 /*
