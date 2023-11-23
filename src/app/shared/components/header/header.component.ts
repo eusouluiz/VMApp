12b2d1112bdf02dc88b18a6598f1e9600b562177
Luiz G. Nascimento <luiznascimento@carbonaut.io>
2023-11-23 17:04:31 -0300

    build: fix android build

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { RealtimeChannel, Subscription, SupabaseClient, createClient } from '@supabase/supabase-js';
+import { Component, Input, OnInit } from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
 import { environment } from '../../../../environments/environment';
 import { ToastService } from '../../../core/toasts/services/toast-service/toast.service';
 import { ConstantesSupabase } from '../../utilities/constantes/constantes.utility';
@@ -16,62 +16,52 @@ export class HeaderComponent implements OnInit {
 
   private static supabase: any;
 
-  constructor(
-    private toastService: ToastService
-  ) {
-    if(HeaderComponent.supabase === undefined){
-      HeaderComponent.supabase = createClient(environment.supabaseUrl, environment.supabaseKey)
+  constructor(private toastService: ToastService) {
+    if (HeaderComponent.supabase === undefined) {
+      HeaderComponent.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
     }
-    this.inscreverNotificacao()
+    this.inscreverNotificacao();
   }
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   // nao precisaria remover os canais, pois esses canais persistem por toda aplicacao
   // OnDestroy(){
   //   HeaderComponent.supabase.removeAllChannels()
   // }
 
-  inscreverNotificacao(){
-    const mensage = HeaderComponent.supabase.channel(ConstantesSupabase.CANAL_MENSAGEM)
-      .on(
-        'postgres_changes',
-        { event: 'INSERT', schema: 'public', table: 'mensagens' },
-        async (payload:any) => {
-          console.log('Mensagem Change received!', payload)
+  inscreverNotificacao() {
+    const mensage = HeaderComponent.supabase
+      .channel(ConstantesSupabase.CANAL_MENSAGEM)
+      .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'mensagens' }, async (payload: any) => {
+        console.log('Mensagem Change received!', payload);
 
-          var nome = await this.getUsuarioNome(payload.new.user_id)
-          console.log(nome)
-          this.toastService.message(nome + ': ' + payload.new.texto)
-        }
-      )
-      .subscribe()
-      const aviso = HeaderComponent.supabase.channel(ConstantesSupabase.CANAL_NOTIFICACAO_AVISO)
-        .on(
-          'postgres_changes',
-          { event: 'INSERT', schema: 'public', table: 'avisos' },
-          async (payload:any) => {
-            console.log('Aviso Change received!', payload)
-            this.toastService.message(payload.new.texto)
-          }
-        )
-        .subscribe()
+        var nome = await this.getUsuarioNome(payload.new.user_id);
+        console.log(nome);
+        this.toastService.message(nome + ': ' + payload.new.texto);
+      })
+      .subscribe();
+    const aviso = HeaderComponent.supabase
+      .channel(ConstantesSupabase.CANAL_NOTIFICACAO_AVISO)
+      .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'avisos' }, async (payload: any) => {
+        console.log('Aviso Change received!', payload);
+        this.toastService.message(payload.new.texto);
+      })
+      .subscribe();
   }
 
-  async getUsuarioNome(id: string): Promise<string>{
-
+  async getUsuarioNome(id: string): Promise<string> {
     let { data: users, error } = await HeaderComponent.supabase
       .from('users')
-      .select("nome")
+      .select('nome')
       // Filters
       .eq('id', id)
-      .single()
+      .single();
 
     if (users !== null) {
-      console.log(users.nome)
-      return users.nome
+      console.log(users.nome);
+      return users.nome;
     }
-    return ''
-
+    return '';
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,16 @@
 export const environment = {
-  production: true,
+  production: false,
   api: {
     endpoint: 'https://vmapi.fly.dev/api',
     maxWaiting: 10000,
     authHeader: 'Authorization',
   },
   log: {
-    enabled: true,
+    enabled: false,
     payload: {},
   },
   availableLanguages: ['br', 'en'],
+  supabaseUrl: 'https://uatspjoodeqhbhubdgrh.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhdHNwam9vZGVxaGJodWJkZ3JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIwNDYyODIsImV4cCI6MjAwNzYyMjI4Mn0.nCCmUhIAtrW0csSC6y_erJ_H7UpaGxXHC6ON4AYk7tY',
 };
